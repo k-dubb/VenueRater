90af605aa95558971317c044a0b4ace8c25e0a07
Kelli Waiss <kelliwaiss@hotmail.com>
2016-02-11 11:04:00 -0600

    fixed ajax to server request

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 var secrets = require('../secrets');
 
 /* GET home page. */
@@ -20,9 +21,34 @@ router.get('/main', function(req, res, next) {
 // });
 
 router.get('/rate/:id', function(req, res, next) {
+    var id = req.params.id;
+    var uri = 'http://api.seatgeek.com/2/venues/';
+
+    request({
+        method: 'GET',
+        uri: uri,
+        qs: {id: id},
+        headers: {encoding: 'utf8', 'Content-type': 'application/json'}
+    }, function(error, response, body) {
+        if (error) throw error;
+        console.log(response.statusCode);
+        var venue = JSON.parse(body);
+        console.log(venue);
+        // res.send('success');
+        res.render('rate', { name: venue.venues[0].name,
+                             address: venue.venues[0].address,
+                             city: venue.venues[0].city,
+                             state: venue.venues[0].state,
+                             zip: venue.venues[0].postal_code,
+                             url: venue.venues[0].url
+
+        });
+        //res.render('show_movie', {title: movie.results[0].display_title});
+    });
+
   // res.render('rate', { venueName: req.params.name, venueAddress: data.venues.address, venueCity: , venueState: , venueZipCode:  });
-  res.render('rate');
-  res.send(req.params.id);
+//   res.render('rate');
+//   res.send(req.params.id);
 });
 
 router.get('/profile', function(req, res, next) {
