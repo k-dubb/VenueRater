b109d55e4cd1b2745a402ea7f20706592c368c1c
Kelli Waiss <kelliwaiss@hotmail.com>
2016-02-12 09:56:42 -0600

    removed all secrets files

--- routes/api/v1/venues.js
+++ routes/api/v1/venues.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-// var secrets = require('../../../secrets');
 var Venue = require('../../../models/venue');
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
-var secrets = require('../secrets');
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
